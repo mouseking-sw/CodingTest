964dca448c4338898bc4ef730a5c26ee7d739aec
조상원 <sang-won@josang-won-ui-MacBookPro.local>
2020-09-16 11:58:13 +0900

    BFS(deque 라이브러리 사용)

--- 5_9_BFS.py
+++ 5_9_BFS.py
@@ -1,15 +1,15 @@
-from collections import deque
+from collections import deque  # deque라이브러리
 
 def bfs(graph,start,visited):
     queue =deque([start])
     visited[start]= True
 
-    while queue:
+    while queue: # queue 빌때 까지
         v = queue.popleft()
         print(v,end=' ')
 
         for i in graph[v]:
-            if not visited[i]:
+            if not visited[i]: #방문하지 않았다면 queue삽입
                 queue.append(i)
                 visited[i]= True
 
@@ -25,5 +25,5 @@ graph = [
     [1,7]
 
 ]
-visited = [False]*9
-bfs(graph,1,visited)
+visited = [False]*9  # 초기 방문자 리스트 False로 초기화
+bfs(graph,1,visited) # 1번 노드 부터 시작
