4cd4a95f30b54b821cd1722579d37acdec0540b4
조상원 <sang-won@josang-won-ui-MacBookPro.local>
2020-09-21 11:02:39 +0900

    순차탐색활용

---
+++ 7_6_떡볶이 떡 만들기(순차탐색).py
@@ -0,0 +1,24 @@
+n,m = map(int,input().split())
+array= list(map(int,input().split()))
+total= [0]*n
+h=0
+# def binary_search(array,target,start,end):
+#     while start <= end:
+#         h= (start+end)//2
+#         if target == array[h]:
+#             return h
+#         elif target > array[h]:
+#             start=h+1
+#         else:
+#             end= h-1
+#     return None
+
+while sum(total)!= m:
+    for i in range(len(array)):
+        total[i]=array[i]-h 
+        if sum(total)==m:
+            break
+    h=h+1
+print(h)
+
+        
